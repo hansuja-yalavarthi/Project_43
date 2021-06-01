aa8c52ed48c6e4fd95bc4435f3750437a7103ead
hansuja-yalavarthi <78600451+hansuja-yalavarthi@users.noreply.github.com>
2021-06-01 12:12:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,15 @@
-var bg,bg2,form,system,code,security;
-var score=0;
+var bg, bg2, form, system, code, security;
+var score = 0;
 
 function preload() {
- 
-  
-  bg= loadImage("aladdin_cave.jpg")
-  //load image for the treasure background
-  
+  bg = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg");
 }
 
 function setup() {
-  createCanvas(1000,500);
-  security = new Security();
+  createCanvas(1000, 500);
   system = new System();
-  
-  
-  
+  security = new Security();
 }
 
 function draw() {
@@ -25,16 +19,14 @@ function draw() {
   textSize(20);
   fill("white");
   text("Score: " + score, 450, 50);
-// add code for changing the background to the treasure background
-  
 
-  if(score === 3) {
-    clear()
-    background(bg2)
-    fill("black")
+  if (score === 3) {
+    clear();
+    background(bg2);
+    fill("white");
     textSize(40);
-    text("TREASURE UNLOCKED",250, 200);
+    text("TREASURE UNLOCKED", 250, 200);
   }
 
-  drawSprites()
+  drawSprites();
 }
