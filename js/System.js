0672b6c49ec5696ac6713e8ccd3d0d20e0088d46
hansuja-yalavarthi <78600451+hansuja-yalavarthi@users.noreply.github.com>
2021-06-01 12:14:00 +0530

    Update System.js

--- js/System.js
+++ js/System.js
@@ -1,14 +1,15 @@
-class System{
+class System {
 
-    constructor(){}
+    constructor() {}
 
-    authenticate(actualCode,enteredCode){
+    authenticate(actualCode, enteredCode) {
         textSize(50);
         fill("black");
-        text(code,300,300)
-        //add code to return true if correct answer entered.
-
-        
+        text(code, 300, 300);
+        if (actualCode === enteredCode.toUpperCase())
+            return true;
+        else
+            return false;
     }
 
 }
